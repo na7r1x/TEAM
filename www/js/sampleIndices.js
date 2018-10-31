3197ada8f805212f2dc36abbc7cb66b48d879a9d
Ivanin Ivanov <na7r1x@gmail.com>
2018-10-31 00:10:40 +0000

    first commit

---
+++ www/js/sampleIndices.js
@@ -0,0 +1,18 @@
+// This file and it's content is automatically generated and should not be altered!
+var samples = [
+    {
+        "path": "www/world/Workbench/index.html",
+        "requiredFeatures": [
+            "image_tracking"
+        ],
+        "startupConfiguration": {
+            "camera_position": "back",
+            "camera_resolution": "auto"
+        }
+    }
+]
+
+function getSamplePath(sample) {
+	return samples[sample];
+
+}
